5c54f73f8d19e9bf3db8d589b28129e660d13e28
Zheng Kai <132241773+skzhengkai@users.noreply.github.com>
2023-06-21 21:17:22 -0700

    i accidentally pushed my bearer

--- vyroai/imagine.js
+++ vyroai/imagine.js
@@ -7,7 +7,7 @@ const Style = require('./constants');
 async function generateImage(prompt, aspect_ratio, style) {
   const url = 'https://api.vyro.ai/v1/imagine/web/generations';
   const headers = {
-    'bearer': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI4MDE5ZWIwMi0xOGFmLTQ3OGMtODg3OS03NzA4YzhlMGQxM2IiLCJpYXQiOjE2ODczODU5NTksImV4cCI6MTY4NzQ3MjM1OX0.1DM9iRSBz1Zy_gqVyvxK9Ra7--tRzmFkOu-4Uoxvs30',
+    'bearer': process.env['BEARER'],
     'style-id': Style[style][0] || '30'
   };
 
@@ -21,10 +21,6 @@ async function generateImage(prompt, aspect_ratio, style) {
   if (styleData) {
     const [styleId, asset, assetPath, suffix] = styleData;
     data.append('style_id', styleId);
-    // You can append the other parameters associated with the style if needed
-    // data.append('asset', asset);
-    // data.append('asset_path', assetPath);
-    // data.append('suffix', suffix);
     if (suffix) {
       prompt += suffix;
     }
@@ -32,19 +28,15 @@ async function generateImage(prompt, aspect_ratio, style) {
 
   console.log(`prompt: ${prompt}`);
 
-  
-
-  axios.post(url, data, { headers, responseType: 'arraybuffer' })
-    .then(response => {
-      fs.writeFileSync('./output.png', Buffer.from(response.data, 'binary'));
-      console.log('Image saved as output.png');
-      return true;
-    })
-    .catch(error => {
-      console.error(error);
-      return false;
-    });
+  try {
+    const response = await axios.post(url, data, { headers, responseType: 'arraybuffer' });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
+
 
 async function upscale(imageData) {
   try {
